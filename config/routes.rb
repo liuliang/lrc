82a4af21efdde7862a26298ab58a3dab6b392bb7
liang.liu <liu_liang_@126.com>
2010-09-21 11:40:09 +0800

    master

--- config/routes.rb
+++ config/routes.rb
@@ -58,6 +58,9 @@ ActionController::Routing::Routes.draw do |map|
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
   # map.root :controller => "welcome"
 
+  #要换成前台的主页
+  map.root :controller => '/admin/dashboards', :action => :index
+
   # See how all your routes lay out with "rake routes"
 
   # Install the default routes as the lowest priority.
